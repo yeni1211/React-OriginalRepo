96d7276ea9dd462088120eb07a81c08255d60c8e
Yesenia Márquez <ymarquez@labsxd.com>
2025-04-10 18:03:08 -0600

    añadiendo estilos al componente StatusMessagesComponent

--- src/components/StatusMessagesComponent.tsx
+++ src/components/StatusMessagesComponent.tsx
@@ -6,10 +6,10 @@ export function StatusMessagesComponent({
   count,
 }: StatusMessagesComponentProps) {
   return (
-    <div>
+    <div className="flex flex-col w-96 mx-auto items-center justify-center bg-gray-300 text-2xl text-gray-800 font-semibold p-8">
       <div>
         {count > 10 ? (
-          <div>El contador es mayor a 10</div>
+          <div>¡El contador es mayor a 10!</div>
         ) : count === 0 ? (
           <div>El contador es 0, por abajo de cero no se puede</div>
         ) : null}
